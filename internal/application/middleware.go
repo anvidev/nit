7e4e4717f918a40effff5d2387f5862c79d652d9
anvi <andreasgylche@gmail.com>
2024-08-19 15:17:59 +0200

    moved middleware functions to its own file

---
+++ internal/application/middleware.go
@@ -0,0 +1,37 @@
+package application
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/anvidev/nit/internal/service"
+)
+
+// withAuth is a middleware that gets the user data from the current cookiestore session,
+// puts it into a [context.Context] from the parent context, and returns shallow copy of
+// the parent context with the user data.
+//
+// This middleware does not care if a user is logged in or not.
+func (app *application) withAuth(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		session, _ := app.service.Store.Get(r, service.CookieKey)
+		user, _ := session.Values[service.UserKey].(service.User)
+
+		ctx := context.WithValue(r.Context(), service.UserKey, user)
+		h.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+// mustAuth is a middleware that gets the current user data from the request context.
+// If no user data is found, it redirects the client to homepage, else it continues.
+func (app *application) mustAuth(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, ok := app.getAuthedUser(r.Context())
+		if !ok {
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
+		h.ServeHTTP(w, r)
+		return
+	})
+}
--- internal/application/routes.go
+++ internal/application/routes.go
@@ -1,10 +1,8 @@
 package application
 
 import (
-	"context"
 	"net/http"
 
-	"github.com/anvidev/nit/internal/service"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -34,27 +32,5 @@ func (app *application) setupRoutes() {
 		r.Post("/projects/create", app.postCreateProject)
 		r.Get("/projects/{id}", app.getProjectByID)
 		r.Delete("/projects/{id}", app.deleteProjectByID)
-	})
-}
-
-func (app *application) withAuth(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		session, _ := app.service.Store.Get(r, service.CookieKey)
-		user, _ := session.Values[service.UserKey].(service.User)
-
-		ctx := context.WithValue(r.Context(), service.UserKey, user)
-		h.ServeHTTP(w, r.WithContext(ctx))
-	})
-}
-
-func (app *application) mustAuth(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, ok := app.getAuthedUser(r.Context())
-		if !ok {
-			http.Redirect(w, r, "/", http.StatusSeeOther)
-			return
-		}
-		h.ServeHTTP(w, r)
-		return
 	})
 }
